4a1a1ceac4b67850e9c7f23831ae0e248229f7a0
MarigonaHamiti <marigona.hamiti@student.uni-pr.edu>
2024-04-20 00:57:43 +0200

    Delete userdelete

--- src/main/java/com/eCommerce/eCommerceApp/Controllers/AuthenticationController.java
+++ src/main/java/com/eCommerce/eCommerceApp/Controllers/AuthenticationController.java
@@ -1,12 +1,11 @@
 package com.eCommerce.eCommerceApp.Controllers;
 
 import com.eCommerce.eCommerceApp.Models.AuthenticationResponse;
-import com.eCommerce.eCommerceApp.Models.Users;
+import com.eCommerce.eCommerceApp.Models.User;
 import com.eCommerce.eCommerceApp.Services.JWTService.AuthenticationService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.security.PublicKey;
 
 @RestController
 @CrossOrigin
@@ -22,14 +21,23 @@ public class AuthenticationController {
     public String hello(){
         return "Hello sir";
     }
-    @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody Users user){
 
-        return ResponseEntity.ok(authenticationService.registerUser(user));
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody User user){
+        // Check if the username is available
+        if (authenticationService.isUsernameAvailable(user.getUsername())) {
+            // If the username is available, proceed with registration
+            AuthenticationResponse response = authenticationService.registerUser(user);
+            return ResponseEntity.ok(response);
+        } else {
+            // If the username is not available, return a conflict response with the appropriate message
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Username is already taken.");
+        }
     }
 
+
     @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> login(@RequestBody Users user){
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody User user){
         return ResponseEntity.ok(authenticationService.authenticate(user));
     }
 
