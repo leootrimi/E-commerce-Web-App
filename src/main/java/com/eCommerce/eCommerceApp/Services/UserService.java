874e3a517cda79f90946d08bc7ee24c868e5590d
MarigonaHamiti <115891512+marigonahamiti@users.noreply.github.com>
2024-04-22 03:17:03 +0200

    deleteUser

--- src/main/java/com/eCommerce/eCommerceApp/Services/UserService.java
+++ src/main/java/com/eCommerce/eCommerceApp/Services/UserService.java
@@ -12,4 +12,6 @@ public interface UserService {
 
     Users findByUsername(String username);
     Users updateUser(String username, Users updateUser);
+    boolean deleteUser(String username);
+    
 }
