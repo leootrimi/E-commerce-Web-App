fcbd5d5ba7b59318311ffa155d1b85ea994d4a79
EndritAbazii <122377055+EndritAbazii@users.noreply.github.com>
2024-05-16 00:44:47 +0200

    FEATURE: Implementimi i sherbimit te karroces per menaxhimin e blerjeve.
    Ky ndryshim ka per qellim te implementoje sherbimin e karroces per menaxhimin e blerjeve ne aplikacionin tone e-commerce

---
+++ src/main/java/com/eCommerce/eCommerceApp/Services/CartService.java
@@ -0,0 +1,25 @@
+package com.eCommerce.eCommerceApp.Services;
+
+import com.eCommerce.eCommerceApp.Models.Cart;
+import com.eCommerce.eCommerceApp.Models.CartItem;
+import com.eCommerce.eCommerceApp.Models.Product;
+import com.eCommerce.eCommerceApp.Models.Users;
+
+import java.util.List;
+
+public interface CartService {
+    // Merr karrocen per nje perdorues
+    Cart getCartByUser(Users user);
+
+    // Shton nje produkt ne karroce
+    void addToCart(Users user, Product product, int quantity);
+
+    // Heq nje produkt nga karroca
+    void removeFromCart(Users user, Product product);
+
+    // Pastron karrocen
+    void clearCart(Users user);
+
+    // Merr listen e artikujve ne karrocen e nje perdoruesi
+    List<CartItem> getCartItems(Users user);
+}
