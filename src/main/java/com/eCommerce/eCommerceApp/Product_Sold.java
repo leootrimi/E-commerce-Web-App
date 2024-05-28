f3dfa2928fc00dc29acbd17c08e55ed3905d8c2a
arbnoraqorraj <115947900+arbnoraqorraj@users.noreply.github.com>
2024-05-28 23:01:56 +0200

    Add files via upload

---
+++ src/main/java/com/eCommerce/eCommerceApp/Product_Sold.java
@@ -0,0 +1,82 @@
+package com.eCommerce.eCommerceApp.Models;
+
+import jakarta.persistence.*;
+
+
+@Entity
+public class Product_Sold {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private Double price;
+
+    private String description;
+
+    private String image;
+
+
+    private int category;
+
+    private String username;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
